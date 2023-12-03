f248fa67cf4ceb889cfd8d53ec6c98c7ac3fb7ab
Samcoding5854 <samcoding5854@gmail.com>
2023-12-03 19:15:33 +0530

    Routing tested

---
+++ frontend/app/contactUs/page.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const contactUs = () => {
+
+  return (
+    <>
+    <div> yoyo </div>
+    </>
+     
+  )
+}
+
+export default contactUs
---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+    <Link href='/contactUs'>
+    <button>Click here Babe</button>
+    </Link>
+    
+    
+    </>
+  )
+}
